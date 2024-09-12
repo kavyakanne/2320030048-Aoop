7fd8cb2bc18edbd09b0915d265065ea78e6cc032
kavyakanne <kavyakanne22@gmail.com>
2024-09-12 11:10:31 +0530

    exercise-5b(b.3)

---
+++ co1/Practical/LSP/src/LSP/Bird.java
@@ -0,0 +1,8 @@
+package LSP;
+
+public class Bird {
+	  public void fly() {
+	        System.out.println("This bird is flying.");
+	    }
+
+}
---
+++ co1/Practical/LSP/src/LSP/Sparrow.java
@@ -0,0 +1,8 @@
+package LSP;
+
+public class Sparrow extends Bird{
+	public void fly() {
+        System.out.println("The sparrow is flying.");
+    }
+
+}
