cc11f2ac9292f439a0766d644691e3d455c32ec5
kavyakanne <kavyakanne22@gmail.com>
2024-09-12 10:52:28 +0530

    exercise-5b(b.2)

---
+++ co1/Practical/OCP/src/ocp/Circle.java
@@ -0,0 +1,13 @@
+package ocp;
+
+public class Circle extends Shape{
+	 private double radius;
+
+	    public Circle(double radius) {
+	        this.radius = radius;
+	    }
+	    public double calculateArea() {
+	        return Math.PI * radius * radius; 
+	    }
+
+}
---
+++ co1/Practical/OCP/src/ocp/Shape.java
@@ -0,0 +1,6 @@
+package ocp;
+
+public abstract class Shape {
+	 public abstract double calculateArea();
+
+}
